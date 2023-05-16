2544042abe415cb48f4ecd0212957ada45f253b5
Wahid Mokhfi <wahid.mokhfi@lapiscine.pro>
2023-05-17 00:24:59 +0200

    17/05/2023

--- src/App.js
+++ src/App.js
@@ -1,31 +1,56 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Home from "./pages/Home"
-import Accueil from "./pages/accueil/Accueil"
-import Apropos from "./pages/apropos/Apropos"
-import Services from "./pages/serv/Services"
-import Tarifs from "./pages/tarifs/Tarifs"
-import Avis from "./pages/avis/Avis"
-import Contact from "./pages/contact/Contact"
-import Connexion from "./pages/connexion/Connexion"
-import "./App.css"
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Accueil from "./pages/accueil/Accueil";
+import Apropos from "./pages/apropos/Apropos";
+import Services from "./pages/serv/Services";
+import Tarifs from "./pages/tarifs/Tarifs";
+import Avis from "./pages/avis/Avis";
+import Contact from "./pages/contact/Contact";
+import Connexion from "./pages/connexion/Connexion";
+import "./App.css";
+
+import ServicesList from "./pages/admin/ServicesList";
+import CreateService from "./pages/admin/CreateService";
+import ServiceDetails from "./pages/admin/ServiceDetail";
+import UpdateService from "./pages/admin/UpdateService";
 
 
 function App() {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/accueil" element={<Accueil />} />
-            <Route path="/apropos" element={<Apropos />} />
-            <Route path="/serv" element={<Services />} />
-            <Route path="/tarifs" element={<Tarifs />} />
-            <Route path="/avis" element={<Avis />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/connexion" element={<Connexion />} />
-        </Routes>
-        {/* <Link to="/connexion">Se connecter</Link> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/services" element={<ServicesList />} />
+        <Route path="/services/:id" element={<ServiceDetails />} />
+
+      
+
+        <Route path="/admin/*" element={<AdminRoutes />} />
+
+        <Route path="/accueil" element={<Accueil />} />
+        <Route path="/apropos" element={<Apropos />} />
+        <Route path="/serv" element={<Services />} />
+        <Route path="/tarifs" element={<Tarifs />} />
+        <Route path="/avis" element={<Avis />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/connexion" element={<Connexion />} />
+      </Routes>
     </BrowserRouter>
   );
 }
 
+function AdminRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<ServicesList />} />
+      <Route path="/create-service" element={<CreateService />} />
+      <Route path="/:id" element={<ServiceDetails />} />
+      <Route path="/:id/update" element={<UpdateService />} />
+    </Routes>
+  );
+}
+
 export default App;
+
--- src/pages/connexion/Connexion.jsx
+++ src/pages/connexion/Connexion.jsx
@@ -1,39 +1,75 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./connexion.css";
 
 function Connexion() {
   const [isLogin, setIsLogin] = useState(true);
+  const [successMessage, setSuccessMessage] = useState("");
+  const navigate = useNavigate(); // Ajout de l'importation et de la constante navigate
 
   const handleSwitchForm = () => {
     setIsLogin(!isLogin);
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const username = event.target.username.value;
+    const password = event.target.password.value;
+
+    fetch("http://localhost:3000/api/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const jwt = data.token;
+        localStorage.setItem("jwt", jwt);
+        setSuccessMessage("Connexion réussie !");
+        navigate("/avis"); // Utilisation de navigate pour la navigation
+      })
+      .catch((error) => {
+        console.error("Erreur lors de la connexion :", error);
+      });
+  };
+
   return (
     <div className="form-container-custom">
-      <form className="form-custom">
+      <form className="form-custom" onSubmit={handleSubmit}>
         <h2>{isLogin ? "Connexion" : "Inscription"}</h2>
-        <label htmlFor="email">Adresse email :</label>
-        <input type="email" id="email" required />
-        <label htmlFor="password">Mot de passe :</label>
+        <label htmlFor="username">Username :</label>
+        <input type="text" id="username" required />
+        <label htmlFor="password">Password :</label>
         <input type="password" id="password" required />
         {!isLogin && (
           <>
-            <label htmlFor="confirm-password">Confirmation du mot de passe :</label>
+            <label htmlFor="confirm-password">Confirm Password :</label>
             <input type="password" id="confirm-password" required />
           </>
         )}
         <button type="submit">{isLogin ? "Se connecter" : "S'inscrire"}</button>
         <div className="switch-form-custom" onClick={handleSwitchForm}>
-
-        <div class="form-bottom">
-          <span>{isLogin ? "Pas encore de compte ?" : "Déjà inscrit ?"}</span>
-          <button type="button" class="auth-btn">{isLogin ? "S'inscrire" : "Se connecter"}</button>
-
-        </div>
+          <div className="form-bottom">
+            <span>{isLogin ? "Pas encore de compte ?" : "Déjà inscrit ?"}</span>
+            <button type="button" className="auth-btn">
+              {isLogin ? "S'inscrire" : "Se connecter"}
+            </button>
+          </div>
         </div>
       </form>
+      {successMessage && <p>{successMessage}</p>}
     </div>
   );
 }
 
 export default Connexion;
+
+
+
+
+
